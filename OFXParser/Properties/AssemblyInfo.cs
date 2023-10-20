1315d3271e926ce37ffd0e5234e96d077170fe23
Leonardo Melo Santos <leonardomelosantos@gmail.com>
2023-10-19 23:47:23 -0300

    Updated Assembly

--- OFXParser/Properties/AssemblyInfo.cs
+++ OFXParser/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OFXParser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library that transforms OFX files. NET objects stating only the file path.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Leonardo Melo Santos")]
 [assembly: AssemblyProduct("OFXParser")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
