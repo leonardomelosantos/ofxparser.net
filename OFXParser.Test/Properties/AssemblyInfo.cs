51ec018960935f61f516cc1004a43adfb624605d
Leonardo Melo Santos <leonardomelosantos@gmail.com>
2023-10-19 23:25:05 -0300

    Uploaded Nuget Spec file

--- OFXParser.Test/Properties/AssemblyInfo.cs
+++ OFXParser.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OFXParser.Test")]
+[assembly: AssemblyTitle("OFXParser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OFXParser.Test")]
+[assembly: AssemblyProduct("OFXParser")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
