b64d4b8de7e85d9d9fe89ddb27941bb471cede42
ismaelReis <ismael.csr@gmail.com>
2024-01-22 19:58:55 -0300

    add: extract ofx file balance (LEDGERBAL tags)

---
+++ OFXParser/Entities/Balance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OFXParser.Entities
+{
+    public class Balance
+    {
+        public double Value { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
